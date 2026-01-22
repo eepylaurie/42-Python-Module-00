c0fcb929834fe49d33c387b946f6ccaa603e3442
eepylaurie <eepylaurie@gmail.com>
2026-01-22 23:08:00 +0100

    ex0 - ex7

---
+++ ex5/ft_count_harvest_recursive.py
@@ -0,0 +1,11 @@
+def print_days(i, days_until_harvest):
+    if i > days_until_harvest:
+        return
+    print("Day", i)
+    print_days(i + 1, days_until_harvest)
+
+
+def ft_count_harvest_recursive():
+    days_until_harvest = int(input("Days until harvest: "))
+    print_days(1, days_until_harvest)
+    print("Harvest time!")
